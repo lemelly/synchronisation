efd3df564766284028f4ff457f3b2a2d28f265fe
Eliane Lemire <ellyanelemire@gmail.com>
2017-10-03 21:35:03 -0400

    ajout des fichiers

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8">
+	<title>Le titre du document</title>
+	<meta name="author" content="Eliane">
+	<meta name="description" content="Short Description of the page contents for SEO purposes">
+	<meta name="viewport" content="width=device-width,initial-scale=0.1">
+	<link rel="stylesheet" href="style/main.css">
+</head>
+
+<body>
+
+	<h1></h1>
+
+</body>
+
+</html>
